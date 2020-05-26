554ff6106402ca78e983186e3861a63ca915debd
Joaquin Piñeyro <joaquin.pineyro@mercadolibre.com>
2020-05-26 15:57:42 -0300

    tf_ec2_instance_1590519461248.tf

---
+++ tf_ec2_instance_1590519461248.tf
@@ -0,0 +1,25 @@
+
+    resource "aws_instance" "vi-aws-test" { # Es una buena práctica que el tag Name coincida con el nombre del recurso en Terraform
+      ami                    = "ami-08e14f4cd60e63c75"
+      instance_type          = "m5.xlarge"
+      iam_instance_profile   = "CloudSec-SSM-EC2-EC2DefaultRoleInstanceProfile"
+      subnet_id              = "subnet-7eba6c50" # Datos propios de la cuenta AWS
+      vpc_security_group_ids = "sg-05e144687873ff85c"   # Datos propios de la cuenta AWS
+      tags = {
+        Name         = "vi-aws-test"
+        team         = "is-cloud"
+        creator      = "jpineyro"
+        requester    = "jpineyro"
+        owner        = "jpineyro"
+        ad-joined    = "true"
+        environment  = "test"
+        owner-team   = "is-cloud"
+        platform     = "ubuntu1804"
+        roles        = "test PR"
+        created      = "whale"
+      }
+      credit_specification {
+        cpu_credits = "unlimited"
+      }
+    }
+   
